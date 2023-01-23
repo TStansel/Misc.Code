aab566e9d1a2920b615c2adbf4832456c412f71f
Thomas Stansel <thomas.s.stansel@gmail.com>
2023-01-22 19:38:15 -0700

    Add functionality for pushing a book to a gist

--- index.js
+++ index.js
@@ -8,16 +8,81 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import axios from "axios";
-const BOOKSURL = "https://www.googleapis.com/books/v1/volumes?";
-const apiKey = process.env.booksAPIKey;
+import { Octokit } from "@octokit/rest";
+const booksURL = "https://www.googleapis.com/books/v1/volumes?";
+const booksAPIKey = process.env.booksAPIKey;
+const githubAPIToken = process.env.githubAPIToken;
+const gistID = process.env.gistID;
 export const handler = (event) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(event);
     let exampleTxt = "red rising";
-    let completeURL = BOOKSURL + "q=" + exampleTxt + "&key=" + apiKey;
-    const booksResponse = yield axios.get(completeURL);
-    console.log(booksResponse.data);
-    return buildResponse(200, "Book added to list!");
+    let exampleNotes = "great one";
+    let completeURL = booksURL + "q=" + exampleTxt + "&key=" + booksAPIKey;
+    const book = yield getBookInformation(completeURL, exampleNotes);
+    const updateResonse = yield updateGist(githubAPIToken, gistID, book);
+    return buildResponse(200, updateResonse);
 });
+function getBookInformation(url, notes) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let booksResponse;
+        try {
+            booksResponse = yield axios.get(url);
+        }
+        catch (error) {
+            console.error(`Unable to access Google Books API:\n${error}`);
+        }
+        const bookData = booksResponse.data.items[0].volumeInfo;
+        const title = bookData.title;
+        const author = bookData.authors[0];
+        const publisher = bookData.publisher;
+        return {
+            title: title,
+            author: author,
+            publisher: publisher,
+            notes: notes,
+        };
+    });
+}
+function updateGist(githubAPIToken, gistID, book) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const octokit = new Octokit({
+            auth: `token ${githubAPIToken}`,
+        });
+        let gist;
+        try {
+            gist = yield octokit.gists.get({ gist_id: gistID });
+        }
+        catch (error) {
+            console.error(`Unable to get gist:\n${error}`);
+        }
+        // There should be just a single file
+        const filename = Object.keys(gist.data.files)[0];
+        const fileContent = gist.data.files[filename].content;
+        const newContentLine = "\n| " +
+            book.title +
+            " | " +
+            book.author +
+            " | " +
+            book.publisher +
+            " | " +
+            book.notes +
+            " |";
+        try {
+            yield octokit.gists.update({
+                gist_id: gistID,
+                files: {
+                    [filename]: {
+                        content: fileContent + newContentLine,
+                    },
+                },
+            });
+        }
+        catch (error) {
+            console.error(`Unable to update gist:\n${error}`);
+        }
+        return "Book added successfully!";
+    });
+}
 function buildResponse(status, body) {
     let sendHeaders = {
         "content-type": "application/json",
@@ -28,6 +93,5 @@ function buildResponse(status, body) {
         headers: sendHeaders,
         body: body,
     };
-    console.log(response);
     return response;
 }
--- index.ts
+++ index.ts
@@ -4,16 +4,19 @@ import {
   APIGatewayProxyStructuredResultV2,
 } from "aws-lambda";
 import axios from "axios";
+import { Octokit } from "@octokit/rest";
 
 type Book = {
-    title: string;
-    author: string;
-    publisher: string;
-    notes: string;
-  };
+  title: string;
+  author: string;
+  publisher: string;
+  notes: string;
+};
 
-const BOOKSURL = "https://www.googleapis.com/books/v1/volumes?";
-const apiKey = process.env.booksAPIKey as string;
+const booksURL = "https://www.googleapis.com/books/v1/volumes?";
+const booksAPIKey = process.env.booksAPIKey as string;
+const githubAPIToken = process.env.githubAPIToken as string;
+const gistID = process.env.gistID as string;
 
 export const handler = async (
   event: APIGatewayProxyEventV2
@@ -21,18 +24,24 @@ export const handler = async (
   console.log(event);
   let exampleTxt = "red rising";
   let exampleNotes = "great one";
-  let completeURL = BOOKSURL + "q=" + exampleTxt + "&key=" + apiKey;
+  let completeURL = booksURL + "q=" + exampleTxt + "&key=" + booksAPIKey;
 
-  const book = getBookInformation(completeURL, exampleNotes);
+  const book = await getBookInformation(completeURL, exampleNotes);
 
-  return buildResponse(200, "Book added to list!");
+  const updateResonse = await updateGist(githubAPIToken, gistID, book);
+
+  return buildResponse(200, updateResonse);
 };
 
 async function getBookInformation(url: string, notes: string): Promise<Book> {
-  // TODO: Add try catch and error handling for axios call
-  const booksResponse = await axios.get(url);
+  let booksResponse: any;
+  try {
+    booksResponse = await axios.get(url);
+  } catch (error) {
+    console.error(`Unable to access Google Books API:\n${error}`);
+  }
 
-  const bookData = booksResponse.data.volumeInfo;
+  const bookData = booksResponse.data.items[0].volumeInfo;
   const title = bookData.title;
   const author = bookData.authors[0];
   const publisher = bookData.publisher;
@@ -43,6 +52,52 @@ async function getBookInformation(url: string, notes: string): Promise<Book> {
     publisher: publisher,
     notes: notes,
   } as Book;
+}
+
+async function updateGist(
+  githubAPIToken: string,
+  gistID: string,
+  book: Book
+): Promise<string> {
+  const octokit = new Octokit({
+    auth: `token ${githubAPIToken}`,
+  });
+
+  let gist: any;
+  try {
+    gist = await octokit.gists.get({ gist_id: gistID });
+  } catch (error) {
+    console.error(`Unable to get gist:\n${error}`);
+  }
+
+  // There should be just a single file
+  const filename = Object.keys(gist.data.files)[0];
+  const fileContent = gist.data.files[filename].content;
+  const newContentLine =
+    "\n| " +
+    book.title +
+    " | " +
+    book.author +
+    " | " +
+    book.publisher +
+    " | " +
+    book.notes +
+    " |";
+
+  try {
+    await octokit.gists.update({
+      gist_id: gistID,
+      files: {
+        [filename]: {
+          content: fileContent + newContentLine,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(`Unable to update gist:\n${error}`);
+  }
+
+  return "Book added successfully!";
 }
 
 function buildResponse(
